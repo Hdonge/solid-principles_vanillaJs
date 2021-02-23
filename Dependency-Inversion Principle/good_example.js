f59312cf48ba038b6bf2552d09b6a53694bd47f1
Harshal Donge <harshal.donge@gmail.com>
2021-02-23 18:45:19 +0530

    Update good_example.js

--- Dependency-Inversion Principle/good_example.js
+++ Dependency-Inversion Principle/good_example.js
@@ -1,3 +1,8 @@
+/*
+ *High-level modules should not depend on low-level modules.  Both should depend on abstractions. 
+ *Abstractions should not depend on details.  Details should depend on abstractions.
+ */
+
 class Store {
     constructor(paymentProcessor) {
         this.paymentProcessor = paymentProcessor;
